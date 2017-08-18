16f5fed747ef21b83d78a5f59ae6accf7b6241f3
Francis Wang <francis@yieldnexus.com>
2017-08-18 16:47:29 -0700

    Track clients in one place

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 
 extern crate ws;
 
-use std::rc::Rc;
-use std::cell::Cell;
-
 use ws::{listen, Handler, Sender, Result, Message, Handshake, CloseCode, Error, ErrorKind};
 use ws::util::{Token, Timeout};
+use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::sync::{Mutex, Arc};
 
 #[derive(Debug, Copy, Clone)]
-struct Position {
+struct Player {
     id: u32,
     x: i32,
     y: i32,
@@ -17,21 +17,26 @@ struct Position {
 
 struct Server {
     out: Sender,
-    position: Position,
+    peer_addr: Option<SocketAddr>,
+    players: Arc<Mutex<HashMap<SocketAddr, Player>>>,
 }
 
 const STATE: Token = Token(1);
 
 impl Handler for Server {
-    fn on_open(&mut self, _: Handshake) -> Result<()> {
+    fn on_open(&mut self, hs: Handshake) -> Result<()> {
+        self.peer_addr = hs.peer_addr;
+        let mut players = self.players.lock().unwrap();
+        players.insert(hs.peer_addr.unwrap(), Player { id: 0, x: 0, y: 0 });
         self.out.timeout(100, STATE)
     }
 
     fn on_timeout(&mut self, event: Token) -> Result<()> {
         match event {
             STATE => {
-                let p = &mut self.position;
-                self.out.send(format!("{}:{},{}", p.id, p.x, p.y));
+                if let Some(p) = self.players.lock().unwrap().get(&self.peer_addr.unwrap()) {
+                    self.out.send(format!("{}:{},{}", p.id, p.x, p.y));
+                }
                 self.out.timeout(100, STATE)
             }
             _ => Err(Error::new(ErrorKind::Internal, "Invalid token"))
@@ -46,23 +51,23 @@ impl Handler for Server {
         let mut split = split.next().unwrap().split(',');
         let dx = split.next().unwrap().parse::<i32>().unwrap();
         let dy = split.next().unwrap().parse::<i32>().unwrap();
-        if self.position.id < command_id {
-            self.position.id = command_id;
-            self.position.x += dx;
-            self.position.y += dy;
+        let mut players = self.players.lock().unwrap();
+        if let Some(player) = players.get_mut(&self.peer_addr.unwrap()) {
+            if player.id < command_id {
+                player.id = command_id;
+                player.x += dx;
+                player.y += dy;
+            }
         }
         Ok(())
     }
 }
 
 fn main() {
-    // let position = Rc::new(Cell::new(Position { id: 0, x: 0, y: 0 }));
+    let mut players: Arc<Mutex<HashMap<SocketAddr, Player>>> = Arc::new(Mutex::new(HashMap::new()));
     listen("127.0.0.1:3012", |out| { Server { 
         out: out, 
-        position: Position { 
-            id: 0, 
-            x: 0, 
-            y: 0 
-        } 
+        players: players.clone(),
+        peer_addr: None,
     } }).unwrap();
 } 
